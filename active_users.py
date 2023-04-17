e51648c89d27fa0424fcad11300fd9b9a7814505
o.druska <o.druska@fz-juelich.de>
2023-04-17 15:36:14 +0200

    implemented argparse to provide servername and access token

--- active_users.py
+++ active_users.py
@@ -1,27 +1,40 @@
 import requests as rq
+import argparse
+
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="This script ist supposed to retrieve information about the usage of Matrix accounts.\n" +
+                                        "Running this script could be useful to find abandoned accounts.")
+    parser.add_argument('-t', '--token', help="Provide an admin-token to access the Matrix API.", required=True)
+    parser.add_argument('-s', '--server', help="Provide the home address of your matrix server.", required=True)
+
+    return parser
+
+def get_profile_info(username, domain):
+    return rq.get(url= f"https://{server}/_matrix/client/v3/profile/@{username}:{domain}", headers=headers)
+
+
+def get_presence_status(username, domain):
+    return rq.get(url= f"https://{server}/_matrix/client/v3/presence/@{username}:{domain}/status", headers=headers)
+
 
 def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    global token
+    token = args.token
+
+    global server
+    server = args.server
+
+    global headers
+    headers = {"Authorization": f"Bearer {token}"}
+
     username = "oskar.d"
     domain = "inm7.de"
 
-    print(username)
-    print(domain)
-
-    headers = {
-        "Authorization": "<censored>" # You have to provide your own accestoken (Im not gonna push my own token here hehe)
-    }
-    print(headers)
-
-    tobias_profile = rq.get(url=f"https://inm7.modular.im/_matrix/client/v3/profile/@tobias:inm7.de", headers=headers)
-    print(tobias_profile)
-    print(tobias_profile.json())
-
-    oskar_presence_status = rq.get(
-        url = f"https://inm7.modular.im/_matrix/client/v3/presence/@{username}:{domain}/status",
-        headers = headers
-    )
-    print(oskar_presence_status)
-    print(oskar_presence_status.json())
+    print(get_profile_info(username=username, domain=domain).json())
+    print(get_presence_status(username=username, domain=domain).json())
 
 
 if __name__ == "__main__":
