8198f17c6bc60d01138643e6bad71e8b1d523cbb
o.druska <o.druska@fz-juelich.de>
2023-05-05 14:03:38 +0200

    (WIP) modifying headers bc API has trouble with processing dictionaries

--- active_users.py
+++ active_users.py
@@ -7,25 +7,36 @@ import json
 
 logger = logging.getLogger()
 
+
 def create_parser() -> argparse.ArgumentParser:
-    '''
+    """
     creates an ArgumentParser object which will be used to analyze given command line arguments
         Input: None
         Return: argparse.ArgumentParser
-    '''
-    parser = argparse.ArgumentParser(description="This script ist supposed to retrieve information about the usage of Matrix accounts.\n" +
-                                                 "Running this script could be useful to find abandoned accounts.")
+    """
+    parser = argparse.ArgumentParser(
+        description="This script ist supposed to retrieve information about the usage of Matrix accounts.\n" +
+                    "Running this script could be useful to find abandoned accounts.")
 
     # User has to sepcify exactly one argument, either an access token
     # XOR username and password to retrieve a token via this script
     login = parser.add_mutually_exclusive_group(required=True)
 
-    login.add_argument('-t', '--token', help="Provide an admin-token to access the Matrix API.")
-    login.add_argument('-l', '--login', help="Provide a username and password to be used in authentication process.", nargs=2, type=str)
-    login.add_argument('-d', '--dummy', help="Makes a dummy call to the API login; API will return a dummy access token", action='store_true', default=False)
+    login.add_argument('-t', '--token',
+                       help="Provide an admin-token to access the Matrix API.")
+    login.add_argument('-l', '--login',
+                       help="Provide a username and password to be used in authentication process.",
+                       nargs=2, type=str)
+    login.add_argument('-d', '--dummy',
+                       help="Makes a dummy call to the API login; API will return a dummy access token",
+                       action='store_true', default=False)
 
-    parser.add_argument('-s', '--matrix_server', help="Provide the home address of your matrix matrix_server.", required=True)
-    parser.add_argument('-a', '--ascending', help="List the accounts in ascending order => most recently online first", default=False, action='store_true', required=False)
+    parser.add_argument('-s', '--matrix_server',
+                        help="Provide the home address of your matrix matrix_server.",
+                        required=True)
+    parser.add_argument('-a', '--ascending',
+                        help="List the accounts in ascending order => most recently online first",
+                        default=False, action='store_true', required=False)
 
     # not sure how the API version is important to our efforts.
     # the current versions are hardcoded based on examples I found on the Matrix API documentation.
@@ -33,8 +44,11 @@ def create_parser() -> argparse.ArgumentParser:
 
     return parser
 
+
 def make_request_call(request_method: str, url: str, headers: dict) -> dict:
-    response = rq.request(method=request_method, url=url, headers=headers)
+    response = rq.request(method=request_method.capitalize(),
+                          url=url,
+                          headers=headers)
 
     if response.status_code != 200:
         logger.error("Fehler: Die API-Anfrage ist fehlgeschlagen.\n" +
@@ -47,12 +61,12 @@ def make_request_call(request_method: str, url: str, headers: dict) -> dict:
 
 
 def get_users(matrix_server: str, headers: dict) -> dict:
-    '''
+    """
     Uses requests to call Matrix API to get a list of registered user objects.
         Input:  str: matrix_server
                 dict: headers
         Return: dict
-    '''
+    """
 
     url = f"https://{matrix_server}/_synapse/admin/v2/users"
 
@@ -60,7 +74,7 @@ def get_users(matrix_server: str, headers: dict) -> dict:
 
 
 def get_access_token(_login_type: str, matrix_server: str, **kwargs) -> str:
-    '''
+    """
     Takes a type and (username, password) or (token) to retrieve an access token via API call.
     _login_type is the login method. Has to be retrieved via a GET call to the login page of the matrix server
     m.login.password -> usr_pwd login scheme (will use usr_pwd to login)
@@ -72,16 +86,17 @@ def get_access_token(_login_type: str, matrix_server: str, **kwargs) -> str:
                 str: _token
         Return: str: access_token
         Failure: if token call not successful, then exit program.
-    '''
+    """
     # Listing the registered users is only possible with admin access
 
-    print(kwargs)
+    print("get_access_token kwargs:\n" + str(kwargs))
 
     _usr = kwargs.get('usr', None)
     _pwd = kwargs.get('pwd', None)
     _token = kwargs.get('token', None)
 
     url = f"https://{matrix_server}/_matrix/client/r0/login"
+    print("login url: " + str(url))
 
     match _login_type:
         case "m.login.password":
@@ -89,48 +104,59 @@ def get_access_token(_login_type: str, matrix_server: str, **kwargs) -> str:
         case "m.login.token":
             d = {'type': _login_type, 'token': _token}
         case "m.login.dummy":
-            d = {'type' : _login_type}
+            d = {'type': _login_type}
         case _:
-            logger.error(f"Fehler: Die angegebene Login Variante ist nicht gültig: {_login_type}\n" +
-                         "Mögliche Loginvarianten sind: " + str(['m.login.password', 'm.login.token', 'm.login.dummy']))
+            logger.error(
+                f"Fehler: Die angegebene Login Variante ist nicht gültig: {_login_type}\n" +
+                "Mögliche Loginvarianten sind: " + str(
+                    ['m.login.password', 'm.login.token', 'm.login.dummy']))
             exit(1)
-
-    d = json.dumps(d)
 
     return make_request_call('POST', url, d)['access_token']
 
 
-def create_request_header(token: str) -> str:
-    '''
+def create_request_header(token: str) -> dict:
+    """
     Takes a token string and creates a header dictionary as JSON string
     Scheme: {Authorization: Bearer <token>}
         Input: str
         Return: str
-    '''
+    """
 
-    headers = json.dumps({"Authorization": f"Bearer {token}"})
+    headers = {"Authorization": f"Bearer {token}"}
+
     return headers
 
 
 def main() -> None:
     args = create_parser().parse_args()
     matrix_server = args.matrix_server
+    print("args:\n" + str(args))
+    print("Server: " + str(matrix_server))
 
     if args.dummy:
-        args.token = get_access_token(_login_type="m.login.dummy", matrix_server=matrix_server)
+        args.token = get_access_token(_login_type="m.login.dummy",
+                                      matrix_server=matrix_server)
     elif args.login:
-        args.token = get_access_token(_login_type="m.login.password", matrix_server=matrix_server, usr=args.login[0], pwd=args.login[1])
+        args.token = get_access_token(_login_type="m.login.password",
+                                      matrix_server=matrix_server,
+                                      usr=args.login[0], pwd=args.login[1])
     else:
-        args.token = get_access_token(_login_type="m.login.token", matrix_server=matrix_server, token=args.token)
+        args.token = get_access_token(_login_type="m.login.token",
+                                      matrix_server=matrix_server,
+                                      token=args.token)
+
+    print("processed token: " + str(args.token))
 
     headers = create_request_header(args.token)
-    print(headers)
+    print("created headers:\n" + str(headers))
 
     matrix_users = get_users(matrix_server, headers)
     accounts = list(matrix_users)
 
     # sorts the accounts by the last_active_ago timestamps, default: least recently online to most recently online
-    accounts.sort(key=lambda x: int(x.last_active_ago), reverse=(not args.ascending))
+    accounts.sort(key=lambda x: int(x.last_active_ago),
+                  reverse=(not args.ascending))
     print(accounts)
 
 
