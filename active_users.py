008ac3035b0dd018af772d3f26f00f541270d4de
o.druska <o.druska@fz-juelich.de>
2023-05-22 13:21:11 +0200

    adds debug output and reworks request_call function

--- active_users.py
+++ active_users.py
@@ -27,15 +27,16 @@ def create_parser() -> argparse.ArgumentParser:
     login.add_argument('-l', '--login',
                        help="Provide a username and password to be used in authentication process.",
                        nargs=2, type=str)
-    login.add_argument('-d', '--dummy',
-                       help="Makes a dummy call to the API login; API will return a dummy access token",
-                       action='store_true', default=False)
 
     parser.add_argument('-s', '--matrix_server',
                         help="Provide the home address of your matrix matrix_server.",
                         required=True)
     parser.add_argument('-a', '--ascending',
                         help="List the accounts in ascending order => most recently online first",
+                        default=False, action='store_true', required=False)
+    parser.add_argument('-d', '--debug',
+                        help="Debug mode; will print more verbose debug "
+                             "output",
                         default=False, action='store_true', required=False)
 
     # not sure how the API version is important to our efforts.
@@ -45,10 +46,21 @@ def create_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def make_request_call(request_method: str, url: str, headers: dict) -> dict:
-    response = rq.request(method=request_method.capitalize(),
+def make_request_call(request_method: str, url: str, data: dict) -> dict:
+    """
+    Incapsualtes a common rq.request() call by adding a status code check and
+    error messages
+    :param request_method HTTP verb
+    :param url URL to call on
+    :param data python dictionary containing
+    """
+    logger.debug(request_method)
+    logger.debug(url)
+    logger.debug(data)
+
+    response = rq.request(method=request_method,
                           url=url,
-                          headers=headers)
+                          data=json.dumps(data))
 
     if response.status_code != 200:
         logger.error("Fehler: Die API-Anfrage ist fehlgeschlagen.\n" +
@@ -64,7 +76,7 @@ def get_users(matrix_server: str, headers: dict) -> dict:
     """
     Uses requests to call Matrix API to get a list of registered user objects.
         Input:  str: matrix_server
-                dict: headers
+                dict: data
         Return: dict
     """
 
@@ -89,14 +101,14 @@ def get_access_token(_login_type: str, matrix_server: str, **kwargs) -> str:
     """
     # Listing the registered users is only possible with admin access
 
-    print("get_access_token kwargs:\n" + str(kwargs))
+    logger.debug("get_access_token kwargs:\n" + str(kwargs))
 
     _usr = kwargs.get('usr', None)
     _pwd = kwargs.get('pwd', None)
     _token = kwargs.get('token', None)
 
     url = f"https://{matrix_server}/_matrix/client/r0/login"
-    print("login url: " + str(url))
+    logger.debug("login url: " + str(url))
 
     match _login_type:
         case "m.login.password":
@@ -111,6 +123,8 @@ def get_access_token(_login_type: str, matrix_server: str, **kwargs) -> str:
                 "Mögliche Loginvarianten sind: " + str(
                     ['m.login.password', 'm.login.token', 'm.login.dummy']))
             exit(1)
+
+    logger.debug(d)
 
     return make_request_call('POST', url, d)['access_token']
 
@@ -131,13 +145,14 @@ def create_request_header(token: str) -> dict:
 def main() -> None:
     args = create_parser().parse_args()
     matrix_server = args.matrix_server
-    print("args:\n" + str(args))
-    print("Server: " + str(matrix_server))
 
-    if args.dummy:
-        args.token = get_access_token(_login_type="m.login.dummy",
-                                      matrix_server=matrix_server)
-    elif args.login:
+    if args.debug:
+        logger.setLevel('DEBUG')
+
+    logger.debug("args:\n" + str(args))
+    logger.debug("Server: " + str(matrix_server))
+
+    if args.login:
         args.token = get_access_token(_login_type="m.login.password",
                                       matrix_server=matrix_server,
                                       usr=args.login[0], pwd=args.login[1])
@@ -149,7 +164,7 @@ def main() -> None:
     print("processed token: " + str(args.token))
 
     headers = create_request_header(args.token)
-    print("created headers:\n" + str(headers))
+    print("created data:\n" + str(headers))
 
     matrix_users = get_users(matrix_server, headers)
     accounts = list(matrix_users)
